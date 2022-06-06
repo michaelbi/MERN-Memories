0d7c5b3d83be9f29ef51b8f2f1b4d8d41931a973
Michael <10m.b10@gmail.com>
2022-06-06 21:27:47 +0300

    new route

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 //define router to the parent route /posts
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
-app.use("/users", (req, res) => {
+app.use("/", (req, res) => {
   res.send("Wellcom to memories");
 });
 
